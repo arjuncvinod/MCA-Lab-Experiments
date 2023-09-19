e1501f73a0bf3a3916fd62f2530e91172026dd28
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-19 22:51:57 +0530

    python program added

---
+++ C/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+  // printf("Enter the details of student %d",i+1);
+    // printf("\nRoll NO : ");
+    // scanf("%d",&s[i].rollno);
---
+++ Python/SumOfTwoNum.py
@@ -0,0 +1,4 @@
+num1=input("Enter the first Number: ")
+num2=input("Enter the second Number: ")
+sum=int(num1)+int(num2)
+print("Sum is",sum)
