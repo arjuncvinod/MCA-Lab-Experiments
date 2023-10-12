d7a19ce402f758488fe85ca6368a5d257e9faddd
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-10-12 22:56:32 +0530

    renamed

--- Python/Area of a circle.py
+++
@@ -1,2 +0,0 @@
-radius = float(input("Enter the radius of circle : "))
-print("The area of given circle is : ",3.14*(radius*radius))
--- Python/SumOfTwoNum.py
+++
@@ -1,4 +0,0 @@
-num1=input("Enter the first Number: ")
-num2=input("Enter the second Number: ")
-sum=int(num1)+int(num2)
-print("Sum is",sum)
