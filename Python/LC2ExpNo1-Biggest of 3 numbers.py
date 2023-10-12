3b214c84a600a1ae1469f810e90402be13a5b1eb
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-10-12 22:59:11 +0530

    LC2 added

---
+++ Python/LC2ExpNo1-Biggest of 3 numbers.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    greatest = num1
+elif num2 >= num1 and num2 >= num3:
+    greatest = num2
+else:
+    greatest = num3
+
+print("The greatest number among", num1, ",", num2, ", and", num3, "is", greatest)
+
