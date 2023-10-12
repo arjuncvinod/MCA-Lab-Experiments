414681919e2e1109da191342f97b90ae7878144b
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-10-12 22:58:44 +0530

    LC1 added

---
+++ Python/LC1ExpNo5-Salary of an employee.py
@@ -0,0 +1,5 @@
+basic_pay = float(input("Enter the basic pay : "))
+house_rent_allowance = 0.10*basic_pay
+travel_allowance = 0.05*basic_pay
+print("The total salary of the employee is ",
+basic_pay+house_rent_allowance+travel_allowance)
