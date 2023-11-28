944aa5fb0d29a07d99ea85b1738b198634589c4f
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-29 01:34:09 +0530

    php added

---
+++ Web/PHP/ExpNo05.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Day of the Week</title>
+</head>
+<body>
+
+<h2>Enter a Day Number</h2>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Enter Day Number (1-7): <input type="text" name="dayNumber">
+    <input type="submit" value="Show Day">
+</form>
+
+<?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the entered day number
+    $dayNumber = $_POST["dayNumber"];
+
+    // Use switch to determine the day of the week
+    switch ($dayNumber) {
+        case 1:
+            echo "Day: Monday";
+            break;
+        case 2:
+            echo "Day: Tuesday";
+            break;
+        case 3:
+            echo "Day: Wednesday";
+            break;
+        case 4:
+            echo "Day: Thursday";
+            break;
+        case 5:
+            echo "Day: Friday";
+            break;
+        case 6:
+            echo "Day: Saturday";
+            break;
+        case 7:
+            echo "Day: Sunday";
+            break;
+        default:
+            echo "Invalid number. Please enter a number between 1 and 7.";
+    }
+}
+?>
+
+</body>
+</html>
