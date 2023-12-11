e25869425966181043b4b347849e7004719318b3
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-12-11 19:26:13 +0530

    LC5 addded

---
+++ Python/Modules/graphics/threeDgraphics/cuboid.py
@@ -0,0 +1,7 @@
+# graphics/threeDgraphics/cuboid.py
+
+def surface_area(length, width, height):
+    return 2 * (length * width + width * height + height * length)
+
+def volume(length, width, height):
+    return length * width * height
