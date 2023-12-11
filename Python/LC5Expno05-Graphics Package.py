08914333d0faaf875cbeedbf0a7e5a268ed67e7e
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-12-11 19:25:57 +0530

    LC5 addded

---
+++ Python/LC5Expno05-Graphics Package.py
@@ -0,0 +1,27 @@
+# Example program using selective import and user inputs
+
+from Modules.graphics.rectangle import area as rect_area, perimeter as rect_perimeter
+from Modules.graphics.circle import area as circle_area, perimeter as circle_perimeter
+from Modules.graphics.threeDgraphics.cuboid import surface_area as cuboid_surface_area, volume as cuboid_volume
+
+# Taking user inputs for rectangle
+length_rect = float(input("Enter the length of the rectangle: "))
+width_rect = float(input("Enter the width of the rectangle: "))
+
+# Taking user input for circle
+radius_circle = float(input("Enter the radius of the circle: "))
+
+# Taking user input for cuboid
+length_cuboid = float(input("Enter the length of the cuboid: "))
+width_cuboid = float(input("Enter the width of the cuboid: "))
+height_cuboid = float(input("Enter the height of the cuboid: "))
+
+# Calculating and displaying results
+print("\nRectangle Area:", rect_area(length_rect, width_rect))
+print("Rectangle Perimeter:", rect_perimeter(length_rect, width_rect))
+
+print("\nCircle Area:", circle_area(radius_circle))
+print("Circle Perimeter:", circle_perimeter(radius_circle))
+
+print("\nCuboid Surface Area:", cuboid_surface_area(length_cuboid, width_cuboid, height_cuboid))
+print("Cuboid Volume:", cuboid_volume(length_cuboid, width_cuboid, height_cuboid))
