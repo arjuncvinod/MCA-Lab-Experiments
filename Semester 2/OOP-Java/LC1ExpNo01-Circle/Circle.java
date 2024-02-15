bc8479594e3753f50fe3fa74567ae0004666a39c
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-15 23:09:34 +0530

    LC1Expno1 added

---
+++ Semester 2/OOP-Java/LC1ExpNo01-Circle/Circle.java
@@ -0,0 +1,33 @@
+public class Circle {
+    float radius;
+
+    Circle() {
+        this.radius = 1;
+    }
+
+    Circle(float radius) {
+        this.radius = radius;
+    }
+
+    float calculateCircumference() {
+        return (float) (2 * Math.PI * radius);
+    }
+
+    float getRadius() {
+        return radius;
+    }
+
+    void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public static void main(String[] args) {
+        Circle circleDefault = new Circle();
+        System.out.println("Default Circle Radius: " + circleDefault.getRadius());
+
+        Circle circleParameterized = new Circle(5);
+        System.out.println("Parameterized Circle Radius: " + circleParameterized.getRadius());
+
+        System.out.println("Circumference of Parameterized Circle: " + circleParameterized.calculateCircumference());
+    }
+}
