0a2a93a89d5b41a8b621d2c2d530c794a6fb5299
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-16 00:03:43 +0530

    LC1Expno5 updated

--- Semester 2/OOP/LC1ExpNo05-Method Overloading/AreaCalculator.java
+++ Semester 2/OOP/LC1ExpNo05-Method Overloading/AreaCalculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class AreaCalculator {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Select a shape to calculate area:");
@@ -15,24 +15,24 @@ class AreaCalculator {
         switch (choice) {
             case 1:
                 System.out.print("Enter the radius of the circle: ");
-                double radius = scanner.nextDouble();
-                System.out.println("Area of the circle: " + calculateArea(radius));
+                float radiusCircle = scanner.nextFloat();
+                System.out.println("Area of the circle: " + calculateAreaCircle(radiusCircle));
                 break;
 
             case 2:
                 System.out.print("Enter the length of the rectangle: ");
-                double length = scanner.nextDouble();
+                float lengthRectangle = scanner.nextFloat();
                 System.out.print("Enter the width of the rectangle: ");
-                double width = scanner.nextDouble();
-                System.out.println("Area of the rectangle: " + calculateArea(length, width));
+                float widthRectangle = scanner.nextFloat();
+                System.out.println("Area of the rectangle: " + calculateAreaRectangle(lengthRectangle, widthRectangle));
                 break;
 
             case 3:
                 System.out.print("Enter the base of the triangle: ");
-                double base = scanner.nextDouble();
+                float baseTriangle = scanner.nextFloat();
                 System.out.print("Enter the height of the triangle: ");
-                double height = scanner.nextDouble();
-                System.out.println("Area of the triangle: " + calculateAreaTriangle(base, height));
+                float heightTriangle = scanner.nextFloat();
+                System.out.println("Area of the triangle: " + calculateAreaTriangle(baseTriangle, heightTriangle));
                 break;
 
             default:
@@ -42,15 +42,15 @@ class AreaCalculator {
         scanner.close();
     }
 
-    static double calculateArea(double radius) {
-        return Math.PI * radius * radius;
+    static float calculateAreaCircle(float radius) {
+        return (float) (Math.PI * radius * radius);
     }
 
-    static double calculateArea(double length, double width) {
+    static float calculateAreaRectangle(float length, float width) {
         return length * width;
     }
 
-    static double calculateAreaTriangle(double base, double height) {
-        return 0.5 * base * height;
+    static float calculateAreaTriangle(float base, float height) {
+        return 0.5f * base * height;
     }
 }
