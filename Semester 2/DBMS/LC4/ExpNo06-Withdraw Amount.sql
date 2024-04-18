a7979db64abe70c56755ec3129d23cf354bf795b
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-04-18 23:21:03 +0530

    LC4expno 6 updated

--- Semester 2/DBMS/LC4/ExpNo06-Withdraw Amount.sql
+++ Semester 2/DBMS/LC4/ExpNo06-Withdraw Amount.sql
@@ -7,7 +7,6 @@ CREATE PROCEDURE WithdrawAmount (
 BEGIN
     DECLARE current_balance DECIMAL(10, 2);
     
-    -- Check if the account exists
     SELECT balance INTO current_balance
     FROM accounts
     WHERE id = account_id;
@@ -15,9 +14,7 @@ BEGIN
     IF current_balance IS NULL THEN
         SELECT 'Account does not exist' AS 'Message';
     ELSE
-        -- Check if new balance after withdrawal is at least 1000
         IF current_balance - withdraw_amount >= 1000 THEN
-            -- Perform withdrawal
             UPDATE accounts
             SET balance = balance - withdraw_amount
             WHERE id = account_id;
